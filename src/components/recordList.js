007f27b1c0360d1aa861ef298b7a1cc6e983b849
Connor Humbert <connor.humbert@ukg.com>
2022-09-19 15:07:59 -0400

    changed create new cocktail because of mobile

--- src/components/recordList.js
+++ src/components/recordList.js
@@ -4,7 +4,7 @@ import Dropdown from './Dropdown';
 import Form from "./Form";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-
+import { NavLink } from "react-router-dom";
  
 const Record = (props) => (
  <tr>
@@ -168,6 +168,7 @@ const handleDeleteFalse = () => {
  return (
    <div>
      <h3>Cocktail List</h3>
+     
      <p></p>
      <Dropdown
           label="Filter by Booze "
@@ -198,6 +199,10 @@ const handleDeleteFalse = () => {
       </Modal>
       <p></p>
       <p>Current Total # of Cocktails: {records.length}</p>
+      <NavLink className="nav-link" to="/create">
+               Create New Cocktail (Click Here)
+             </NavLink>
+      <p></p>
      <table className="table table-striped" style={{ marginTop: 20 }}>
        <thead>
          <tr>
@@ -214,174 +219,3 @@ const handleDeleteFalse = () => {
    
  );
 }
-// import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import Header from './Header';
-// import Dropdown from './Dropdown';
-// import Button from "./Button";
-// import Cocktail from "./Cocktail";
-// import Form from "./Form";
-// import Matrix from "./Matrix";
-// import Popup from "./Popup";
-
-// export default function RecordList() {
-
-
-
-//   const [alcohol, setAlcohol] = React.useState("None"); //liquor dropdown
-//   const [showChange, setShowChange] = React.useState(false); //inital show button
-//   const [drinksArray, setDrinksArray]= React.useState([]); //main array of drinks to be shown
-//   const [noFilterSpecified, setNoFilterSpecified] = React.useState(true); //set to false if any filter is specified
-//   const [isOpen, setIsOpen] = useState(false); //for popup
-//   const [filterStr, setFilterStr] = useState("None");
-
-//   //filter by Ingredient form
-//   const onSaveIngredient = (formValue) => {
-
-//     //set alcohol filter to none if filtering by ingredient
-//     setAlcohol("None");
-//     setNoFilterSpecified(false);
-//     setFilterStr(formValue.text);
-
-
-//     let tempArray = [];
-//     let text = formValue.text.toLowerCase();
-
-//     for (var i = 0; i < Drinks.length; i++) { //loop thorugh objects
-
-//       for (const [key, value] of Object.entries(Drinks[i])) {
-//         if (key === "ingredients"){
-//           value.forEach(element => {
-//             if (element.toLowerCase().includes(text)){
-
-//               tempArray.push(Drinks[i].name);
-
-//               }
-//             });
-//           }
-//         }
-//       }
-//       setDrinksArray(Drinks.filter((value) => tempArray.includes(value.name))
-//       .map((val,index) => (
-//         <Cocktail key={index} cocktail ={val}/>
-//       )));
-
-
-//   };
-
-//   //filter by name form
-//   const onSaveName = (values) => {
-
-//     //set alcohol to none if filtering by name
-//     setAlcohol("None");
-//     setNoFilterSpecified(false);
-//     setFilterStr(values.text);
-
-//     setDrinksArray(Drinks.filter((value) => value.name.toLowerCase().includes(values.text.toLowerCase()))
-//     .map((val,index) => (
-//       <Cocktail key={index} cocktail ={val}/>
-//     )));
-
-//   };
-
-//   const options = [
-//     { label: 'None', value: 'None' },
-//     { label: 'Tequila', value: 'Tequila' },
-//     { label: 'Whiskey/Rye/Bourbon', value: "Whiskey/Rye/Bourbon"},
-//     { label: 'Rum', value: 'Rum' },
-//     { label: 'Gin', value: 'Gin' },
-//     { label: 'Vodka', value: 'Vodka' },
-//     { label: 'Wine', value: 'Wine' },
-//   ];
-
-//   //for dropdown
-//   const handleAlcoholChange = (event) => {
-
-//     setAlcohol(event.target.value);
-//     setFilterStr(event.target.value);
-
-//     if (event.target.value === "None") { //dont filter
-//       setDrinksArray(Drinks
-//       .map((val,index) => (
-//         <Cocktail key={index} cocktail ={val}/>
-//       )));
-//     } else {//filter by alcohol
-//       setDrinksArray(Drinks.filter((value) => value.booze === event.target.value)
-//       .map((val,index) => (
-//         <Cocktail key={index} cocktail ={val}/>
-//       )));
-//     }
-//   };
-
-//   //initial on show button
-//   const onShow = () => {
-//     if (alcohol === "None" && noFilterSpecified) { //on initial start, if nothing selected show everything
-//       setDrinksArray(Drinks
-//       .map((val,index) => (
-//         <Cocktail key={index} cocktail ={val}/>
-//       )));
-//     }
-
-//     setShowChange(true);
-//   }
-  
-//   //for pop up
-//   const togglePopup = () => {
-//     setIsOpen(!isOpen);
-//   }
- 
-//   const infoText = "This application was created as a gift for my family. As of June 23rd 2022, the catalog currently contains 125 cocktails.\n That being said, we're big fans of trying new ones. If you use this application and wish to see more drinks added to it please email connor.catalog@gmail.com a cocktail in this example format: \n Gin and Tonic \n 2 oz gin \n 1/2 oz lime juice \n 4-6 oz tonic \n Garnish: Lime wedge "; 
-
-
-//   return (
-//     <section className = "container">
-
-//       <div className ="left-half">
-//       <div>
-//         {/* <FaInfo onClick={togglePopup} className="icon-right"></FaInfo> */}
-//         {/* <FaInfoCircle onClick={togglePopup} className="icon-right"></FaInfoCircle> */}
-
-//         {isOpen && <Popup
-//           content={<> 
-//           <body>Hi!</body>
-//           <div className="display-linebreak"> 
-//             {infoText} 
-//           </div>
-
-//           </>} 
-//           handleClose={togglePopup}
-//         />}
-//       </div>
-//       <div className="divCenter">
-//       <Header/>
-//       <Dropdown
-//           label="Filter by Liquor "
-//           options={options}
-//           value={alcohol}
-//           onChange={handleAlcoholChange}
-//         />
-//         <p> </p> 
-//       <Form
-//           onSave = {onSaveIngredient} nameForm = 'Filter by Ingredient '
-//       />
-//       <Form
-//           onSave = {onSaveName} nameForm = 'Filter by Name '
-//       />
-
-
-//       {!showChange ?  <Button
-//           color = 'black' text = 'Start' onClick={onShow}
-//       /> : ''}
-      
-//       {showChange && filterStr!=="None"? <p>Showing results based on: {filterStr}</p>: ''}
-//       {showChange ? <Matrix cells={drinksArray} /> : <p>Click Start</p>}
-
-//       </div>
-
-//       </div>
-
-//     </section>
-
-
-//   );
-// }
