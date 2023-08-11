a297b889a5c255a81804babe4bd7d0f0c491d043
Connor Humbert <connor.humbert@ukg.com>
2023-08-11 15:02:24 -0400

    swapped clear and submit buttons

--- src/components/ParentForm.js
+++ src/components/ParentForm.js
@@ -81,8 +81,8 @@ const ParentForm = ({ onSave, clearFilters }) => {
         </div>
         <p></p>
         <div className="button-container">
-        <button type="button" onClick={handleClear} className="btn btn-outline-secondary" style={{ marginRight: '10px', marginLeft: '75px' }}>Clear</button>
-          <input type="submit" value="Submit" className="btn btn-outline-primary"  />
+          <button type="button" onClick={handleClear} className="btn btn-outline-secondary" style={{ marginRight: '10px', marginLeft: '75px' }}>Clear</button>
+          <input type="submit" value="Submit" className="btn btn-outline-primary" />
         </div>
       </form>
     </div>
